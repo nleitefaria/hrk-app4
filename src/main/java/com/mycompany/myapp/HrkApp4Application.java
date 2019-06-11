0e4813c4b124d37b198abfdf4b3dc4c11cb7c702
nleitefaria <nleitefaria@gmail.com>
2019-06-11 11:48:05 +0100

    INIT

---
+++ src/main/java/com/mycompany/myapp/HrkApp4Application.java
@@ -0,0 +1,13 @@
+package com.mycompany.myapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrkApp4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrkApp4Application.class, args);
+	}
+
+}
